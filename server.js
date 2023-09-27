ef46acd38c21f7873ccdde7a2ac6e33d098de31e
smshack <5432tat@naver.com>
2023-09-27 10:23:13 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3002;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
